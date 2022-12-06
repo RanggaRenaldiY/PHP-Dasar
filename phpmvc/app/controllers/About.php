abcef2372faf19326068f3fa8d2cfd1c37f25bc9
Rangga Renaldi Yusuf <ranggarenaldi@gmail.com>
2022-12-06 15:11:40 +0700

    Controller

---
+++ phpmvc/app/controllers/About.php
@@ -0,0 +1,10 @@
+<?php
+
+class About {
+    public function index($hari ='pagi', $nama='world'){
+        echo "Selamat $hari, selamat beraktifivitas $nama";
+    }
+    public function page(){
+        echo 'about/page';
+    }
+}
